9efbb7bc8b0e456110b2bf526eda7cc59a27c85f
Euler-37 <1943133009@qq.com>
2021-10-20 19:16:47 +0800

    change:main.f90 test update

--- app/main.f90
+++ app/main.f90
@@ -1,7 +1,19 @@
 program main
     use qblas
     implicit none
-    real(kind=16)::a(3)=1._16
-    real(kind=16),external::qdot
-    write(*,*)qdot(3, a, 1, a, 1)
+    block
+        real(kind=16)::a(3)
+        complex(kind=16)::b(3)
+        real(kind=16),external::qdot
+        complex(kind=16),external::odotc,odotu
+        call random_number(a)
+        call random_number(b%im)
+        call random_number(b%re)
+        write(*,*)"qblas:qdot ",qdot(3, a, 1, a, 1)
+        write(*,*)"exact      ",dot_product(a, a)
+        write(*,*)"qblas:odotc",odotc(3, b, 1, b, 1)
+        write(*,*)"exact      ",dot_product(b, b)
+        write(*,*)"qblas:odotu",odotu(3, b, 1, b, 1)
+        write(*,*)"exact      ",dot_product(conjg(b), b)
+    end block
 end program main
